5aeac09710aa43a65629f6caf90c0a0fb961def9
Goran Vrbaški <vrbaski.goran@gmail.com>
2016-05-31 14:23:31 +0200

    added test

--- sajt/tests.py
+++ sajt/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from sajt.models import *
 
-# Create your tests here.
+
+class SubscriberTest(TestCase):
+
+	def setUp(self):
+		Subscriber.objects.create(name="Petar Petrovic", email="petar.petrovic@nomail.com", is_active=True)
+
+
+	def test_subscriber(self):
+		subscriber = Subscriber.objects.get(email="petar.petrovic@nomail.com")
+
